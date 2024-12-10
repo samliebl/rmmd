e977a9630c55965ee063096d8454e194596e7d05
S T <stliebl@gmail.com>
2024-12-10 17:08:13 -0500

    Adding option to turn on or off custom markup.

--- bin/rmmd.js
+++ bin/rmmd.js
@@ -17,6 +17,7 @@ program
   .version(packageJson.version, '-v, --version', 'Output the version number')
   .option('-f, --file <path>', 'Write output to a file instead of stdout')
   .option('-e, --enclose', 'Wrap output in a fully compliant HTML document')
+  .option('-c, --custom', 'Enable all custom syntax processing')
   .argument('[filepath]', 'Path to a Markdown file to process (optional)', null);
 
 program.parse(process.argv);
@@ -42,17 +43,17 @@ async function main() {
       process.exit(1);
     }
 
-    let output = await markdownToHtml(input);
+    // Process the Markdown with or without custom syntax
+    const output = await markdownToHtml(input, { useCustomSyntax: options.custom });
 
-    if (options.enclose) {
-      output = wrapInHtmlDocument(output);
-    }
+    // Optionally wrap in a full HTML document
+    const finalOutput = options.enclose ? wrapInHtmlDocument(output) : output;
 
     if (options.file) {
-      await writeFile(options.file, output, 'utf8');
+      await writeFile(options.file, finalOutput, 'utf8');
       console.log(`HTML output written to ${options.file}`);
     } else {
-      stdout.write(output);
+      stdout.write(finalOutput);
     }
   } catch (err) {
     console.error('Error:', err.message);
